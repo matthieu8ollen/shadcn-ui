b0324f020fdf7fcd30fe5938fa0291baf7a6cedd
matthieu8ollen <matthieu@cybermindsagency.com>
2025-09-08 17:50:39 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,6 @@
 
 import Login from '@/components/Login'
 
-// Add to your login page
-useEffect(() => {
-  console.log('VERCEL ENV CHECK:')
-  console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
-  console.log('Key exists:', !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-  console.log('Key preview:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.substring(0, 20))
-}, [])
 export default function LoginPage() {
   return <Login />
 }
