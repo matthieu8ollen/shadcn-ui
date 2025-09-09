928a75e796d427358642eb4c2c5b64957b9376a4
matthieu8ollen <matthieu@cybermindsagency.com>
2025-09-09 13:16:38 +0200

    Update page.tsx

--- app/chat/marcus/page.tsx
+++ app/chat/marcus/page.tsx
@@ -273,7 +273,7 @@ const pollForAIResponse = async (sessionId: string) => {
                 </div>
               </div>
             </CardHeader>
-            <CardContent className="flex-1 flex flex-col p-0">
+            <CardContent className="flex-1 flex flex-col p-0 relative">
               <div className="flex-1 overflow-y-auto p-4 space-y-4" style={{maxHeight: 'calc(100vh - 280px)'}}>
                 {currentStatus && (
   <div className="px-4 py-2 bg-blue-50 border-b">
@@ -301,8 +301,8 @@ const pollForAIResponse = async (sessionId: string) => {
                   </div>
                 ))}
               </div>
-              <div className="border-t p-4">
-                <div className="flex gap-2">
+              <div className="border-t p-4 bg-white sticky bottom-0">
+              <div className="flex gap-2">
                   <Input
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
