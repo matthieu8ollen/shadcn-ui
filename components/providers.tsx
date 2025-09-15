60bd93c1ff9b5792ac22b31318782153d9991fbc
matthieu8ollen <matthieu@cybermindsagency.com>
2025-09-15 14:59:02 +0200

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,17 +1,19 @@
 'use client'
-
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from "@/contexts/AuthContext"
+import { ContentProvider } from "@/contexts/ContentContext"  // Add this import
 import { AuthGuard } from "@/components/auth-guard"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <AuthGuard>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </AuthGuard>
+      <ContentProvider>  {/* Add ContentProvider wrapper */}
+        <AuthGuard>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </AuthGuard>
+      </ContentProvider>  {/* Close ContentProvider */}
     </AuthProvider>
   )
 }
